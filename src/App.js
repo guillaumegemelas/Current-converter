d84b26f81ec7db462b33a4daf0adaa14dec0f379
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-02-04 23:04:08 +0100

    final1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import rates from "./assets/data";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faArrowDown, faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import logo from "./img/logo.png";
+// import logo from "./img/logo.png";
 library.add(faArrowDown, faArrowUp);
 //
 
